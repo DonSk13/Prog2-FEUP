f497f9e02587f59a7955bc632225dbc1a2a72165
DonSk13 <37479013+DonSk13@users.noreply.github.com>
2018-03-18 14:39:03 +0000

    Add files via upload

--- MT1 Exemplo 2/ficheiros/prob1/prob1.c
+++ MT1 Exemplo 2/ficheiros/prob1/prob1.c
@@ -1,179 +1,168 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "vetor.h"
 #include "lista.h"
-#include "fila.h"
 
 /****************************************************/
 /*              Funcoes a implementar               */
 /****************************************************/
 
 /*** problema 1.1 ***/
-lista* encontra_nomes(lista *lst1, lista *lst2)
+float comprimento_medio(lista *lst)
 {
-	/* complexidade do algoritmo: O(N²) - Um ciclo for dentro de outro ciclo for */
+	/* complexidade do algoritmo: O(n) - Só existe um ciclo na função implementada */
 
-	l_elemento *nome1, *nome2;
-	//int aux2;
-	lista *aux = lista_nova();
-
-	if(lst1 == NULL || lst2 == NULL)
+	//Verificação de parâmetros
+	if(lst == NULL || lista_tamanho(lst) == 0)
 	{
-		//printf("Erro na leitura das listas\n");
-		return NULL;
+		//printf("Erro!");
+		return 0;
 	}
 
-	for (int i=0; i < lst1->tamanho; ++i)	
+	//Iniciação de variáveis
+	int i, len, size;
+	float avg, total=0;
+	l_elemento *aux;
+
+	size = lista_tamanho(lst); //Atribui a size o tamanho da lista
+	//printf("Tamanho da lista: %d\n", lista_tamanho(lst));
+
+	//ciclo para percorrer a lista
+	for (i=0; i < lst->tamanho; ++i)
 	{
-		nome1 = lista_elemento(lst1, i);
-		
-		for (int j = 0; j < lst2->tamanho; ++j)
-		{
-			nome2 = lista_elemento(lst2, j);
-
-			if (strcmp(nome1->str, nome2->str) == 0)
-			{
-				lista_insere(aux, nome1->str, NULL);	
-			}
-		}
-	}
-	//aux2 = lista_tamanho(aux);
-	//printf("Resultado: %d\n", aux2);
-	return aux;
-
-	//outra hipótese de resolução
-
-	/* complexidade do algoritmo: N^2  */
-
-	/*lista *aux = lista_nova();
-	l_elemento *nome1, *nome2;
-
-	if(lst1 == NULL || lst2 == NULL)
-	{
-		return NULL;
+		aux = lista_elemento(lst, i); //atribui a aux o conteúdo da lista lst na posição i
+		len = strlen(aux->str);	//Atribui a len o comprimento da string
+		//printf("%s\n", aux->str); //Imprime a lista de freguesias
+		//printf("%d\n", len); //Imprime tamanho de cada freguesia
+		total = total + len; //Contabiliza o tamanho total de todas as strings
+		//printf("%d\n", total); //Imprime tamanho total
 	}
 
-	nome1 = lst1->inicio;
-	while (nome1 != NULL)
-	{
-		nome2 = lst2->inicio;
-		while (aux2 != NULL)
-		{
-			if(strcmp(nome1->str,nome2->str) == 0)
-			{
-				lista_insere(aux,nome1->str,NULL);
-			}
-			nome2 = nome2->proximo;
-		}
-		nome1 = nome1->proximo;
-	}
-
-	return aux;*/
+	avg = (total/size);
+	return avg;
 }
 
 /*** problema 1.2 ***/
-int lista_remove_longos(lista *lst, int nomes)
+lista* filtra_nomes(lista *lst, char *existe)
 {
-/*	//Verifica se há erro de leitura das listas
-	if(lst == NULL)
+	//Verificação de parâmetros
+	if(lst == NULL || lista_tamanho(lst) == 0)
 	{
-		//printf("Erro na leitura das listas\n");
-		return -1;
-	}
-
-	//Verifica se lista de entrada tem tamanho zero
-	if (lista_tamanho(lst) == 0)
-	{
-		//printf("Lista de entrada com tamanho zero\n");
-		return -1;
-	}
-
-	int i, aux, j=0;
-	l_elemento *nome;
-
-
-	for (int i=0; i < lst->tamanho; ++i)	
-	{
-		nome = lista_elemento(lst, i);
-		
-    	char *palavra = strtok(nome->str, " "); //Retorna o primeiro espaço
-   
-    	for (aux = 0; palavra != NULL; ++aux)
-    	{
-    		palavra = strtok(NULL, " "); //Conta cada palavra que há em cada string
-    	}
-    	//printf("Palavras:%d\n", aux);
-
-		if (aux>nomes)
-		{
-			//lista_remove(lst, nome);
-			j++;		
-		}
-	}
-	printf("Tamanho da lista: %d\n", lista_tamanho(lst));
-	printf("O número de contactos eliminados é: %d\n", j);
-	return 	j;*/
-	return NULL;
-}
-
-/*** problema 1.3 ***/
-fila* pesquisa_nomes(lista *lst, char *nome)
-{
-	//Verifica se há erro de leitura das listas
-	if(lst == NULL)
-	{
-		//printf("Erro na leitura das listas\n");
+		//printf("Erro!\n");
 		return NULL;
 	}
 
-	//Verifica se lista de entrada tem tamanho zero
-	if (lista_tamanho(lst) == 0)
-	{
-		//printf("Lista de entrada com tamanho zero\n");
-		return NULL;
-	}
-
-	lista *aux = lista_nova();
+	//Iniciação de variáveis
+	lista *aux = lista_nova(); //declaração de lista nova
 	l_elemento *nomes;
 	int i;
+
 	nomes = lista_elemento(lst, i);
 
+	//ciclo para percorrer a lista
 	for (i = 0; i < lst->tamanho; ++i) 
 	{
 		//Apontador para o elemento da lista
-		nomes = lista_elemento(lst, i);
-    	if (strstr(nomes->str, nome) != NULL) 
+		nomes = lista_elemento(lst, i);	//atribui a nomes o conteúdo da lista lst na posição i
+
+		//Se o conteúdo de existe for diferente da string 
+    	if (strstr(nomes->str, existe) != NULL) 
         {
-        	//na lista_insere, NULL dá a posição em que ainda não existe nada e onde pretendemos inserir string
-        	if (lista_insere(aux, nomes->str, NULL) == NULL)
-        	{
-        		return NULL;
-            	//printf("Ocorreu um erro ao inserir uma string na pesquisa parcial\n");
-        	}
+        	lista_insere(aux, nomes->str, NULL);	//Insere esse conteúdo numa nova lista aux
+        	lista_remove(lst, nomes);	//Remove elementos da lista
     	}
 	}
 	return aux;
+    	
+}
+
+/*** problema 1.3 ***/
+const char* mais_palavras(vetor *vec)
+{
+	//Verificação de parâmetros
+	if(vec == NULL || vetor_tamanho(vec) == NULL)
+	{
+		//printf("Erro!\n");
+		return NULL;
+	}
+	
+	//iniciação de variáveis
+	int i, j, max=0, i_max;
+	const char *aux;
+	char *palavra;
+
+	//ciclo para percorrer o vetor enquanto menor que o tamanho
+	for (i = 0; i < vetor_tamanho(vec); i++)
+	{
+		aux = vetor_elemento(vec,i);
+
+		int count = 1; //Para contar a primeira palavra
+
+    	for (j = 0; j < strlen(aux); ++j) //Percorre caractere a caractere o vetor
+    	{
+    		//se encontrar um espaço contabiliza mais uma palavra
+    		if (aux[j] == ' ')
+    		{
+    			count++;
+    		}
+    	}
+    	// se o número de palavras for maior que o máximo
+    	//novo máximo passa a ser o número de palavras atual 
+    	if (count > max) 
+    	{
+    		max = count;
+    		i_max = i;
+    	}
+	}
+
+	vetor_elemento(vec, i_max);
 }
 
 /****************************************************/
 /*     Funcoes ja implementadas (nao modificar)     */
 /****************************************************/
 
+vetor* lerParaVetor(FILE* ficheiro)
+{
+	char buffer[256], *nlptr;
+	vetor* v;
+	
+	if (ficheiro == NULL)
+		return NULL;
+
+	v = vetor_novo();
+
+	while(!feof(ficheiro)) 
+	{	
+		fgets(buffer, 255, ficheiro);
+		nlptr = strchr(buffer, '\n');
+		if (nlptr) 
+			*nlptr = '\0';
+		
+		vetor_insere(v,buffer,v->tamanho);
+	}
+
+	return v;
+}
+
 lista* lerParaLista(FILE* ficheiro)
 {
 	char buffer[256], *nlptr;
 	lista* l;
-
+	
 	if (ficheiro == NULL)
 		return NULL;
 
 	l = lista_nova();
-
-	while(fgets(buffer, 255, ficheiro) != NULL)
-	{
+	
+	while(!feof(ficheiro)) 
+	{	
+		fgets(buffer, 255, ficheiro);
 		nlptr = strchr(buffer, '\n');
-		if (nlptr)
-		*nlptr = '\0';
+		if (nlptr) 
+			*nlptr = '\0';
+		
 		lista_insere(l,buffer,NULL);
 	}
 
@@ -183,84 +172,58 @@ lista* lerParaLista(FILE* ficheiro)
 int main()
 {
 	FILE *f;
-	lista *l1=NULL, *l2=NULL, *l=NULL;
-	fila *resultado=NULL;
-	l_elemento *e;
-	int removidos;
-
+    vetor *v;
+    lista *l, *resultado;
+    l_elemento *e;
+    const char *nome;
+    int i;
+    double comp;
+    
 	/* testes */
-	f = fopen("nomes1.txt","r");
+	f = fopen("freguesias.txt","r");
 	if(f == NULL)
 	{
 		printf("Erro ao ler ficheiro de entrada.\n");
 		return 1;
 	}
-	l1 = lerParaLista(f);
-	fclose(f);
-
-	f = fopen("nomes2.txt","r");
-	if(f == NULL)
-	{
-		printf("Erro ao ler ficheiro de entrada.\n");
-		return 1;
-	}
-	l2 = lerParaLista(f);
-	fclose(f);
-
-	/* inicio teste prob1.1 */
-	l = encontra_nomes(l1, l2);
-	if(l)
-		printf("\nLista resultante contem %d nomes.\n", lista_tamanho(l));
+	l = lerParaLista(f);
+		
+	/* testa comprimento_medio */
+	comp = comprimento_medio(l);
+	if(comp)
+		printf("\nComprimento medio das palavras: %.1f\n", comp);
 	else
-		printf("\nencontra_nomes retornou NULL\n");
-	/* fim teste prob1.1 */
+	   	printf("\ncomprimento_medio retornou 0\n");
 
-	/* inicio teste prob1.2 */
-	removidos = lista_remove_longos(l1, 4);
-	if(removidos!=-1)
-	{
-		printf("\nNomes longos removidos (total %d).\n", removidos);
-		if(lista_tamanho(l1) != 224)
-			printf("Nem todos os nomes longos foram removidos da lista (tamanho: %d; esperado: 19)\n", lista_tamanho(l1));
-	}
-	else
-	printf("\nlista_remove_longos retornou ERRO\n");
-	/* fim teste prob1.2 */
-
-	/* inicio teste prob1.3 */
-	resultado = pesquisa_nomes(l2, "SILVA");
+	/* testa filtra_nomes */
+    resultado = filtra_nomes(l, "Mamede");
+    
 	if(resultado)
 	{
-		int n=0;
-		printf("\nNomes encontrados (%d): \n", fila_tamanho(resultado));
-		while (!fila_vazia(resultado) && n<3) {
-			printf("%s\n", fila_front(resultado));
-			fila_pop(resultado);
-			n++;
-		}
-		if(fila_tamanho(resultado) > 2)
-		{
-			puts("...");
-			while (fila_tamanho(resultado) > 1)
-				fila_pop(resultado);
-			printf("%s\n", fila_front(resultado));
-			fila_pop(resultado);
-		}
-		else {
-			while (!fila_vazia(resultado)) {
-					printf("%s\n", fila_front(resultado));
-					fila_pop(resultado);
-				}
-		}
-	}
+            printf("\nNomes filtrados (total de %d):\n", lista_tamanho(resultado));
+            for (e = resultado->inicio; e != NULL; e = e->proximo)
+                printf("%s\n", e->str);
+
+            if(lista_tamanho(l) != 4230)
+            	printf("Nem todos os nomes foram removidos do vetor (tamanho: %d; esperado: 4218)\n", lista_tamanho(l));
+    }
 	else
-		printf("\npesquisa_nomes retornou NULL\n\n");
-	/* fim teste prob1.3 */
+		printf("\nfiltra_nomes retornou NULL\n");
 
+	/* testa mais_palavras */
+	rewind(f);
+	v = lerParaVetor(f);    
+	nome = mais_palavras(v);
+	if(nome)
+	  	printf("\nNome com mais palavras: %s\n\n", nome);
+	else
+	   	printf("\nmais_palavras retornou NULL\n\n");
+	
+	vetor_apaga(v);
 	lista_apaga(l);
-	lista_apaga(l1);
-	lista_apaga(l2);
-	fila_apaga(resultado);
-
+	lista_apaga(resultado);
+	
+	fclose(f);
+		
 	return 0;
 }
